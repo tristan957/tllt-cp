f40864e7523b62ee77167c58c67f003142d806a0
Tristan Partin <tristan.partin@gmail.com>
2019-02-13 17:02:11 -0600

    Use MESON_INSTALL_PREFIX [ci skip]

--- meson_post_install.sh
+++ meson_post_install.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 if [ -z $DESTDIR ]; then
-	echo $MESON_INSTALL_PREFIX
-	install_prefix="${MESON_INSTALL_PREFIX:-.local}"
 	echo 'Compiling GSchema'
-	glib-compile-schemas "/usr/share/glib-2.0/schemas"
+	glib-compile-schemas "$MESON_INSTALL_PREFIX/share/glib-2.0/schemas"
 	if [ "$1" = "false" ] # Was it lib-only?
 	then
 		echo 'Updating icon cache'
-		gtk-update-icon-cache -qtf "/usr/share/icons/hicolor"
+		gtk-update-icon-cache -qtf "$MESON_INSTALL_PREFIX/share/icons/hicolor"
 		echo 'Updating desktop database'
-		update-desktop-database -q "/usr/share/applications"
+		update-desktop-database -q "$MESON_INSTALL_PREFIX/share/applications"
 	fi
 fi
