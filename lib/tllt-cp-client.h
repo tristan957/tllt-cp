e7acbebed618476b77e31dd3e6f82ca2fe8330df
Tristan Partin <tristan.partin@gmail.com>
2019-03-04 16:33:47 +0000

    Add extern "C"

--- lib/tllt-cp-client.h
+++ lib/tllt-cp-client.h
@@ -2,7 +2,11 @@
 
 #include <glib-object.h>
 
+G_BEGIN_DECLS
+
 #define TLLT_CP_TYPE_CLIENT (tllt_cp_client_get_type())
 G_DECLARE_FINAL_TYPE(TlltCpClient, tllt_cp_client, TLLT_CP, CLIENT, GObject)
 
 TlltCpClient *tllt_cp_client_new_from_environment();
+
+G_END_DECLS
