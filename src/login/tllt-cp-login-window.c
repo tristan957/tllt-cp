db4a759bdce5afce17004e54c821bd42cc130ab5
Tristan Partin <tristan.partin@gmail.com>
2019-03-05 22:30:18 -0600

    Change header bar title when new user button is clicked

--- src/login/tllt-cp-login-window.c
+++ src/login/tllt-cp-login-window.c
@@ -12,6 +12,7 @@ struct _TlltCpLoginWindow
 
 typedef struct TlltCpLoginWindowPrivate
 {
+	GtkHeaderBar *header_bar;
 	GtkStack *login_stack;
 	GtkEntry *login_email_entry;
 	GtkEntry *login_password_entry;
@@ -43,8 +44,12 @@ on_new_user_button_clicked(G_GNUC_UNUSED GtkButton *widget, gpointer user_data)
 {
 	TlltCpLoginWindow *self		   = TLLT_CP_LOGIN_WINDOW(user_data);
 	TlltCpLoginWindowPrivate *priv = tllt_cp_login_window_get_instance_private(self);
+
+	gtk_header_bar_set_title(priv->header_bar, _("Sign Up"));
 	gtk_widget_hide(GTK_WIDGET(widget));
+	gtk_widget_set_can_focus(GTK_WIDGET(widget), FALSE);
 	gtk_stack_set_visible_child_name(priv->login_stack, "new_user_page");
+	gtk_widget_grab_focus(GTK_WIDGET(priv->new_user_name_entry));
 }
 
 static void
@@ -87,6 +92,7 @@ tllt_cp_login_window_class_init(TlltCpLoginWindowClass *klass)
 
 	gtk_widget_class_set_template_from_resource(
 		wid_class, "/com/gitlab/tristan957/tllt-cp/login/tllt-cp-login-window.ui");
+	gtk_widget_class_bind_template_child_private(wid_class, TlltCpLoginWindow, header_bar);
 	gtk_widget_class_bind_template_child_private(wid_class, TlltCpLoginWindow, login_stack);
 	gtk_widget_class_bind_template_child_private(wid_class, TlltCpLoginWindow, login_email_entry);
 	gtk_widget_class_bind_template_child_private(wid_class, TlltCpLoginWindow,
