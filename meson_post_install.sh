b9b4b6225fbc67052fefa7e635cab664d8760495
Tristan Partin <tristan.partin@gmail.com>
2019-03-28 12:12:44 -0500

    use different pkg-config if not found, debian is dumb

--- meson_post_install.sh
+++ meson_post_install.sh
@@ -3,11 +3,8 @@
 if [ -z $DESTDIR ]; then
 	echo 'Compiling GSchema'
 	glib-compile-schemas "$MESON_INSTALL_PREFIX/share/glib-2.0/schemas"
-	if [ "$1" = "false" ] # Was it lib-only?
-	then
-		echo 'Updating icon cache'
-		gtk-update-icon-cache -qtf "$MESON_INSTALL_PREFIX/share/icons/hicolor"
-		echo 'Updating desktop database'
-		update-desktop-database -q "$MESON_INSTALL_PREFIX/share/applications"
-	fi
+	echo 'Updating icon cache'
+	gtk-update-icon-cache -qtf "$MESON_INSTALL_PREFIX/share/icons/hicolor"
+	echo 'Updating desktop database'
+	update-desktop-database -q "$MESON_INSTALL_PREFIX/share/applications"
 fi
