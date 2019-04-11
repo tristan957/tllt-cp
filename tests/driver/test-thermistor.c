005da66af6f1f9a34bd44c0ad8536c45600194e1
Tristan Partin <tristan.partin@gmail.com>
2019-04-11 14:47:07 -0500

    Change up the test

--- tests/driver/test-thermistor.c
+++ tests/driver/test-thermistor.c
@@ -1,14 +1,32 @@
-#include <glib-object.h>
+#ifndef TLLT_WITH_WIRINGPI
+#	error "This test only works if libwiringPi is found"
+#endif
 
-#include "sensors/tllt-sensor.h"
-#include "sensors/tllt-thermistor.h"
+#include <glib-object.h>
+#include <mcp3004.h>
+#include <wiringPi.h>
+
+#define SPI_CHAN 0
+#define BASE_PIN 100
+#define NUM_OF_PINS 8
+#define FOREVER 0
 
 int
 main(G_GNUC_UNUSED int argc, G_GNUC_UNUSED char *argv[])
 {
-	g_autoptr(TlltThermistor) thermistor = tllt_thermistor_new(0, 100, 4);
+	wiringPiSetup();
+	mcp3004Setup(BASE_PIN, SPI_CHAN);
 
-	tllt_sensor_read(TLLT_SENSOR(thermistor));
+	do {
+		int result[NUM_OF_PINS];
+		for (int i = 0; i < NUM_OF_PINS; i++) {
+			result[i] = analogRead(BASE_PIN + i);
+		}
+
+		g_print("%d, %d, %d, %d, %d, %d, %d, %d\n", result[0], result[1], result[2], result[3],
+				result[4], result[5], result[6], result[7]);
+		delay(500);
+	} while (FOREVER);
 
 	return 0;
 }
